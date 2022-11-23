97a95a4fb5a548860234c3b83c78ee485d6058ff
EricAraujo <ericpoloni@gmail.com>
2022-11-23 18:06:12 -0300

    gerando user model do devise

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, :phone, presence: true
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -30,8 +30,8 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
-VCR.configure do |config|
-  config.cassette_library_dir = 'fixtures/vcr'
-  config.hook_into :webmock
-end
+# VCR.configure do |config|
+#   config.cassette_library_dir = 'fixtures/vcr'
+#   config.hook_into :webmock
+# end
 SimpleCov.start
